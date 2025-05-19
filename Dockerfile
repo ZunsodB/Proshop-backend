8b223d494ea47e5900638d3d028dc1fd503c6369
ZunsodB <s21c118b@nmct.edu.mn>
2025-05-19 23:58:07 +0800

    yml and docker file

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:alpine3.20
+WORKDIR package.json ./
+RUN npm install
+COPY . .
+EXPOSE 5000
+CMD ["npm", "run", "start"]
--- index.js
+++ index.js
@@ -8,23 +8,31 @@ import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
+// Load environment variables
 dotenv.config();
+
+// Connect to the database
 connectDB();
 
 const app = express();
+const port = process.env.PORT || 5000;
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Configure CORS
+const allowedOrigins = process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : [];
 app.use(
   cors({
-    origin: ["https://shadowshop.vercel.app"],
+    origin: allowedOrigins,
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
 );
 
-// Add root route
+// Root route
 app.get('/', (req, res) => {
   res.send('Backend API is running');
 });
@@ -33,9 +41,15 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
-app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID || 'Unavailable'));
 
+// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 export default app;
